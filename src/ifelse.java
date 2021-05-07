63b1b97085ebd6bf8b47fb47ce30b880f2aea17f
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-05-07 21:15:07 +0530

    Day 2

---
+++ src/ifelse.java
@@ -0,0 +1,18 @@
+// if else
+public class ifelse {
+	public static void main(String[] args) {
+		int age = 18;
+		if(age>=18) {
+			System.out.println("You are an adult");
+		}
+		else if(age>75) {
+			System.out.println("Okay Boomer");
+		}
+		else if(age>=13) {
+			System.out.println("You are a teenager");
+		}
+		else {
+			System.out.println("not adult");
+		}
+	}
+}
