8e31ebff22cd697e159d9639a905e3bb37334653
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-05-09 13:49:36 +0530

    Day 4

---
+++ src/forLoop.java
@@ -0,0 +1,9 @@
+// for loop
+public class forLoop {
+	public static void main(String[] args) {
+		for(int i=10; i>=10; i--) {
+			System.out.println(i);
+		}
+		System.out.println("Happy new year");
+	}
+}
---
+++ src/whileLoop.java
@@ -0,0 +1,15 @@
+// while loop
+import java.util.Scanner;
+public class whileLoop {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String name = "";
+		
+		while(name.isBlank()) {
+			System.out.print("Enter your name: ");
+			name = scanner.nextLine();
+		}
+		
+		System.out.println("hello "+name);
+	}
+}
