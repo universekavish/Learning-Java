10887096b51d88be8499bbcdc2c748b30927eca2
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-05-13 21:17:56 +0530

    Day 6

---
+++ src/stringMethods.java
@@ -0,0 +1,19 @@
+// string methods
+
+public class stringMethods {
+	public static void main(String[] args) {
+		String name = "Kavish";
+		//boolean result = name.equalsIgnoreCase("Kavish");
+		//int result = name.length();
+		//char result = name.charAt(0);
+		//int result = name.indexOf("a");
+		//boolean result = name.isEmpty();
+		//String result = name.toUpperCase();
+		//String result = name.toLowerCase();
+		//String result = name.trim(); remove empty space in name
+		//String result = name.replace('v', 'a');
+		
+		
+		System.out.println(result);
+	}
+}
