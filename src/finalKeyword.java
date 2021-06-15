62b1a9844090b31caec83ac23a1449ed8f185ab1
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-06-15 14:08:34 +0530

    Day 10

---
+++ src/finalKeyword.java
@@ -0,0 +1,10 @@
+// final keyword
+// anything that is final can not be changed
+public class finalKeyword {
+	public static void main(String[] args) {
+		final double pi = 3.14159;
+		//pi = 4;
+		
+		System.out.println(pi); 
+	}
+}
