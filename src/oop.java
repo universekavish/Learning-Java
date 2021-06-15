35d6256bfce8961c0a617255e846d57175d1f23a
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-06-15 14:30:26 +0530

    Day 10

--- src/oop.java
+++ src/oop.java
@@ -1,10 +1,18 @@
 // objects OOP
 public class oop {
 	public static void main(String[] args) {
-		Car myCar = new Car();
-		System.out.println(myCar.model);
+		Car myCar1 = new Car();
+		Car myCar2 = new Car();
 		
-		myCar.drive();
+		System.out.println(myCar1.model);
+		System.out.println();
+		System.out.println(myCar2.model);
+		
+		myCar1.drive();
+		System.out.println();
+		myCar2.brake();
+		
+		
 	}
 }
 
