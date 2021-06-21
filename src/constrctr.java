1ab754406334a8e9ebbf0e751716b0775d907607
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-06-21 12:50:21 +0530

    Day 11

---
+++ src/constrctr.java
@@ -0,0 +1,10 @@
+// constructor
+public class constrctr {
+	public static void main(String[] args) {
+		Human human1 = new Human("Rick", 65, 70);
+		Human human2 = new Human("Mink", 23, 79);
+		
+		System.out.println(human1.name);
+		System.out.println(human2.name);
+	}
+}
