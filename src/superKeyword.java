dd874fdcb34fac4dfeb75e0b18593b2736583cee
[Kavish Boyal] <38506513+universekavish@users.noreply.github.com>
2021-06-23 13:37:04 +0530

    Day 12

---
+++ src/superKeyword.java
@@ -0,0 +1,10 @@
+// super keyword
+public class superKeyword {
+	public static void main(String[] args) {
+		 Hero hero1 =new Hero("Batman",42,"$$$");
+		 Hero hero2 =new Hero("Superman",43,"everything");
+		
+		 System.out.println(hero2.toString());
+
+	}
+}
